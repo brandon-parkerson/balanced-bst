d160ba058eacd65978d9b502336d3eef92805703
brandon-parkerson <brandon-m-parkerson@gmail.com>
2024-10-04 14:00:03 +0300

    Change file extension. Add print function.

--- index.mjs
+++ index.mjs
@@ -48,5 +48,20 @@ function buildTree(array) {
     node.right = toBst(array, mid + 1, end);
     return node;
   }
+  const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+  prettyPrint(rootNode)
   return tree(rootNode);
 }
+
+
